3e4d478bc91585ebcb3ec4239eb9b66589787a2e
Bib711 <123845805+Bib711@users.noreply.github.com>
2024-08-11 22:21:13 +0530

    Added 2 more tut answers

---
+++ Tut7/InheritanceLab2.java
@@ -0,0 +1,54 @@
+package Lab;
+import java.util.Scanner;
+
+class Vehicle {
+    String model;
+    int year;
+
+    void displayVehicleDetails() {
+        System.out.println("Vehicle Model: " + model);
+        System.out.println("Vehicle Year: " + year);
+    }
+}
+
+class Car extends Vehicle {
+    int noOfDoors;
+
+    void displayCarDetails() {
+        displayVehicleDetails(); // Display vehicle details
+        System.out.println("Number of Doors: " + noOfDoors);
+    }
+}
+
+class ElectricCar extends Car {
+    int batteryCapacity;
+
+    void displayElectricCarDetails() {
+        displayCarDetails(); // Display car details
+        System.out.println("Battery Capacity: " + batteryCapacity + " kWh");
+    }
+}
+
+public class InheritanceLab2 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ElectricCar ec = new ElectricCar();
+
+        // Input details 
+        System.out.print("Enter Vehicle Model: ");
+        ec.model = sc.nextLine();
+        System.out.print("Enter Vehicle Year: ");
+        ec.year = sc.nextInt();
+        System.out.print("Enter Number of Doors: ");
+        ec.noOfDoors = sc.nextInt();
+        System.out.print("Enter Battery Capacity (in kWh): ");
+        ec.batteryCapacity = sc.nextInt();
+
+        // Display the details of the electric car
+        System.out.println("\nElectric Car Details:");
+        ec.displayElectricCarDetails();
+
+        sc.close();
+    }
+}
---
+++ Tut7/InheritanceLab3.java
@@ -0,0 +1,78 @@
+package Lab;
+import java.util.*;
+
+class Emp{
+	String Name = new String();
+	int Age;
+	long phno;
+	String Adrs = new String();
+	void display() {
+		System.out.printf("Name is:"+Name);
+		System.out.printf("\nAge is: %d%n",Age);
+		System.out.printf("Address is:"+Adrs+"\n");
+	}
+}
+class FullTimeEmp extends Emp{
+	float annualSal ;
+	void print_annSal(){
+		display();
+		System.out.print("\nAnnual Salary is:"+annualSal);
+	}
+	
+	
+}
+class PartTimeEmp extends Emp{
+	float hourlyRate;
+	void print_hrlyRat(){
+		display();
+		System.out.print("\nHourly Rate is:"+hourlyRate);
+	}
+}
+public class InheritanceLab3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		FullTimeEmp ft = new FullTimeEmp();
+		PartTimeEmp pt = new PartTimeEmp();
+		
+		System.out.println("Enter FullTime employee Details:\n");
+		System.out.print("Enter Employee name:");
+		ft.Name = sc.nextLine();
+		System.out.print("Enter Employee age:");
+		ft.Age = sc.nextInt();
+		System.out.print("Enter Employee phno:");
+		ft.phno = sc.nextInt();
+		sc.nextLine();
+		System.out.print("Enter Employee Address:");
+		ft.Adrs = sc.nextLine();
+		System.out.print("Enter Employee annual salary:");
+		ft.annualSal = sc.nextFloat();
+		sc.nextLine();
+		
+		System.out.println("Enter PartTime employee Details:\n");
+		System.out.print("Enter Employee name:");
+		pt.Name = sc.nextLine();
+		System.out.print("Enter Employee age:");
+		pt.Age = sc.nextInt();
+		System.out.print("Enter Employee phno:");
+		pt.phno = sc.nextInt();
+		sc.nextLine();
+		System.out.print("Enter Employee Address:");
+		pt.Adrs = sc.nextLine();
+		System.out.print("Enter Employee's hourly rate:");
+		pt.hourlyRate = sc.nextFloat();
+		sc.nextLine();
+		
+		System.out.println("\n\nFullTime Employee Details:\n");
+		ft.display();
+		ft.print_annSal();
+		
+		System.out.println("\n\nPartTime Employee Details:\n");
+		pt.display();
+		pt.print_hrlyRat();
+		
+		sc.close();
+	}
+
+}
